1b8bb19ebc0289e9c8dd0545e4d639592ea752dc
tiara <tiaraoryza111@gmail.com>
2023-02-21 11:14:34 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name"      : "New Addon",
+    "version"  : "1.0",
+    "author"    : "tiara oryza sativa",
+    "website"   : "https://www.detik.com/",
+    "category"  : "New Module",
+    "summary"   : "ini adalah addons baru",
+    "description"   : """ 
+        latihan membuat addons baru di odoo 1 
+    """,
+    "depends"       : [
+        "base",
+    ],
+    "data"          : [
+
+    ],
+    "demo"          :[],
+    "test"          :[],
+    "image"         :[],
+    "qweb"          :[],
+    "css"           :[],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+
+}
